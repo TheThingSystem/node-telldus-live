107e2d7fd672607b866e1bc32adb141f77a3ca5c
Marshall Rose <mrose17@gmail.com>
2014-01-20 16:07:00 -0800

    closer...

--- test.js
+++ test.js
@@ -18,57 +18,64 @@ cloud = new TelldusAPI.TelldusAPI({ publicKey  : publicKey
 
     if (!!err) return console.log('getSensors: ' + err.message);
 
-    f = function(offset, id, s) {
+    f = function(offset, p, s) {
       return function(err, sensor) {
-        if (!!err) return console.log(s + ' id=' + id + ': ' + err.message);
+        var d, prop, type;
 
-        console.log('sensor #' + offset + ' ' + s + ': '); console.log(sensor); console.log('');
+        if (!!err) return console.log(s + ' id=' + p.id + ': ' + err.message);
+
+        console.log('sensor #' + offset + ' ' + s + ': '); console.log(sensor);
+        type = null;
+        for (d in sensor.data) if (sensor.data.hasOwnProperty(d)) {
+          if ((d.name === 'temp') || (d.name === 'humidity')) type = 'meteo';
+        }
+        console.log('/device/climate' + (sensor.protocol || 'telldus') + '/' + (type || 'generic'));
+        console.log('    uuid=teldus:' + sensor.id);
+        console.log('    name: ' + sensor.name);
+        console.log('    status: ' + (p.online === '1' ? 'present' : 'absent'));
+        console.log('    lastSample: ' + sensor.lastUpdated * 1000);
+        console.log('    info:');
+        for (d in sensor.data) if (sensor.data.hasOwnProperty(d)) {
+          prop = { temp : 'temperature' }[d.name] || d.name;
+          console.log('      ' + prop + ': ' + d.value);
+        }
+        console.log('');
       };
     };
 
     console.log('sensors: '); console.log(sensors); console.log(''); console.log('');
-    for (i = 0; i < sensors.length; i++) cloud.getSensorInfo(sensors[i], f(i, sensors[i].id, 'getSensorInfo'));
-
-    var sensor;
-    for (i = 0; i < sensors.length; i++) {
-      sensor = sensors[i];
-      if (sensor.id === '847133') {
-        cloud.setSensorName(sensor, (sensor.name === 'Garaget - ute' ? 'The garage - out' : 'Garaget - ute'),
-                            f(i,sensors[i].id, 'setSensorName'));
-      } else if (sensor.id === '115423') {
-        cloud.setSensorName(sensor, (sensor.name === 'Väderstationen' ? 'Weather Station' : 'Väderstationen'),
-                            f(i,sensors[i].id, 'setSensorName'));
-      }
-      cloud.getSensorInfo(sensors[i], f(i, sensors[i].id));
-    }
+    for (i = 0; i < sensors.length; i++) cloud.getSensorInfo(sensors[i], f(i, sensors[i], 'getSensorInfo'));
   }).getDevices(function(err, devices) {
     var f, i;
 
     if (!!err) return console.log('getDevices: ' + err.message);
 
-    f = function(offset, id, s) {
+    f = function(offset, p, s) {
       return function(err, device) {
-        if (!!err) return console.log(s + ' id=' + id + ': ' + err.message);
+        var d, type;
 
-        console.log('device #' + offset + ' ' + s + ': '); console.log(device); console.log('');
+        if (!!err) return console.log(s + ' id=' + p.id + ': ' + err.message);
+
+        console.log('device #' + offset + ' ' + s + ': '); console.log(device);
+        type = null;
+        d = device.protocol.split(':');
+        type = { 'selflearning-switch' : 'dimmer'
+               , codeswitch            : 'onoff' }[d[0]];
+        if (!type) return;
+        console.log('/device/climate' + (d[d.length - 1] || 'telldus') + '/' + (type || 'generic'));
+        console.log('    uuid=teldus:' + device.id);
+        console.log('    perform: off, on');
+        console.log('    name: ' + device.name);
+        console.log('    status: ' + (device.online === '1' ? 'present' : 'absent'));
+        console.log('    info:');
+        if (type === 'dimmer') console.log('      dimmer: percentage');
+        console.log('');
       };
     };
 
     console.log('devices: '); console.log(devices); console.log(''); console.log('');
-    for (i = 0; i < devices.length; i++) cloud.getDeviceInfo(devices[i], f(i, devices[i].id, 'getDeviceInfo'));
-
-    var device;
     for (i = 0; i < devices.length; i++) {
-      device = devices[i];
-      if (device.id === '127878') {
-        cloud.setDeviceName(device, (device.name === 'Inne 10' ? 'Inside 10' : 'Inne 10'),
-                            f(i,devices[i].id, 'setDeviceName'));
-      } else if (device.id === '127875') {
-        cloud.setDeviceName(device, (device.name === 'Inne: Vardagsrum fönster' ? 'Inside: Living romo window'
-                                                                                : 'Inne: Vardagsrum fönster'),
-                            f(i,devices[i].id, 'setDeviceName'));
-      }
-      cloud.getDeviceInfo(devices[i], f(i, devices[i].id));
+      if (devices[i].type === 'device') cloud.getDeviceInfo(devices[i], f(i, devices[i], 'getDeviceInfo'));
     }
   });
 }).on('error', function(err) {
